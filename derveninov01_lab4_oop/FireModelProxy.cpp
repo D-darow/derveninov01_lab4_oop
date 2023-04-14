0544f14d9d761301aec5790fa211f76561d17f21
Дмитрий Дервенинов <dimader@bk.ru>
2023-04-14 18:40:47 +0700

    Мелкие изменения и добавление комментариев

--- derveninov01_lab4_oop/FireModelProxy.cpp
+++ derveninov01_lab4_oop/FireModelProxy.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "FireModelProxy.h"
 
 FireModelProxy::FireModelProxy(SensorCO* sensorCO, Pyrometr* pyrometr)
@@ -7,61 +8,77 @@ FireModelProxy::FireModelProxy(SensorCO* sensorCO, Pyrometr* pyrometr)
 
 long int FireModelProxy::GetFireDuration()
 {
+	// Здесь может быть реализована логика контроля доступа к объекту FireModel
 	return pFireModel->GetFireDuration();
 }
 
 bool FireModelProxy::StartModeling()
 {
+	// Здесь может быть реализована логика контроля доступа к объекту FireModel
 	return pFireModel->StartModeling();
 }
 
 std::string FireModelProxy::GenerateEmergencyResponseTips()
 {
+	// Здесь может быть реализована логика контроля доступа к объекту FireModel
 	return pFireModel->GenerateEmergencyResponseTips();
 }
 
 std::string FireModelProxy::GeneratePossibleEmergencyConsequences()
 {
+	// Здесь может быть реализована логика контроля доступа к объекту FireModel
 	return pFireModel->GeneratePossibleEmergencyConsequences();
 }
 
 void FireModelProxy::SetEmergencyProbability(double emergencyProbability)
 {
+	// Здесь может быть реализована логика контроля доступа к объекту FireModel
 	pFireModel->SetEmergencyProbability(emergencyProbability);
 }
 
 void FireModelProxy::SetEmergencyScale(std::string emergencyScale)
 {
+	// Здесь может быть реализована логика контроля доступа к объекту FireModel
 	pFireModel->SetEmergencyScale(emergencyScale);
 }
 
 void FireModelProxy::SetPossibleVictims(int possibleVictims)
 {
+	// Пример контроля доступа
+	if (possibleVictims < 0)
+	{
+		std::cout << "Ошибка!" << std::endl;
+	}
 	pFireModel->SetPossibleVictims(possibleVictims);
 }
 
 void FireModelProxy::SetEmergencyHappened(bool emergencyHappened)
 {
+	// Здесь может быть реализована логика контроля доступа к объекту FireModel
 	pFireModel->SetEmergencyHappened(emergencyHappened);
 }
 
 double FireModelProxy::GetEmergencyProbability()
 {
+	// Здесь может быть реализована логика контроля доступа к объекту FireModel
 	return pFireModel->GetEmergencyProbability();
 }
 
 std::string FireModelProxy::GetEmergencyScale()
 {
+	// Здесь может быть реализована логика контроля доступа к объекту FireModel
 	return pFireModel->GetEmergencyScale();
 }
 
 int FireModelProxy::GetPossibleVictims()
 {
+	// Здесь может быть реализована логика контроля доступа к объекту FireModel
 	return pFireModel->GetPossibleVictims();
 }
 
 bool FireModelProxy::GetEmergencyHappened()
 {
+	// Здесь может быть реализована логика контроля доступа к объекту FireModel
 	return pFireModel->GetEmergencyHappened();
 }
 
